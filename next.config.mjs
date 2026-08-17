cca47e3e5df1f1d98dc4ae71fe7adaa48d48e761
lyriqtech-oss <contato@euemoi.com.br>
2026-08-17 20:55:20 -0300

    fix: isolate development build cache folder from production build to prevent compiler conflict

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  distDir: process.argv.includes('dev') ? '.next/dev' : '.next',
+};
 
 export default nextConfig;
